adcad11a5b0e454648cf8ccf0e87a2cf6a25d098
Sanket Gupta <sanketgupta07@gmail.com>
2017-07-23 08:58:10 +0530

    Initial Commit--REST with Spring

---
+++ rest-with-spring/README.md
@@ -0,0 +1,3 @@
+# Building REST application with spring
+
+Ref: https://spring.io/guides/tutorials/bookmarks/
---
+++ rest-with-spring/src/main/java/com/sanket/spring/RestWithSpringApplication.java
@@ -0,0 +1,12 @@
+package com.sanket.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringApplication.class, args);
+	}
+}
